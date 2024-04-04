e8df96ced924e693fe909f23057ff7d2d5c32aa2
Leo <62186360+AlexCole999@users.noreply.github.com>
2024-04-04 20:41:26 +0500

    created picture

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image, Dimensions } from 'react-native';
 
 
 export default function App() {
+
+  const { width } = Dimensions.get('window');
+
   const CustomButton = ({ title, onPress, color }) => {
     return (
       <TouchableOpacity style={[styles.button, { backgroundColor: color }]} onPress={onPress}>
@@ -14,12 +17,17 @@ export default function App() {
 
   const [count, setCount] = useState(0);
   const [state, setState] = useState(0);
+  const [picture, setPicture] = useState("https://randomfox.ca/images/9.jpg");
 
   const handlePress = () => {
     fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
       .then(response => response.json())
       .then(json => { console.log(json); setState(json) })
-    console.log('222');
+    console.log('fox start');
+
+    fetch(`https://randomfox.ca/floof/`)
+      .then(response => response.json())
+      .then(json => { console.log(json); setPicture(json.image) })
   };
   return (
     <View style={styles.container}>
@@ -30,7 +38,10 @@ export default function App() {
       <CustomButton title="Increment" onPress={() => setCount(count + 1)} color="red" />
       <Button title="Decrement" onPress={() => setCount(count - 1)} />
       <Button title="Press me" onPress={handlePress} />
-
+      <Image
+        style={[styles.image, { width: width * 0.9 }]}
+        source={{ uri: picture }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -55,5 +66,11 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: '800',
     textTransform: 'uppercase'
-  }
+  },
+  image: {
+    width: 200,
+    height: 350,
+    resizeMode: 'cover',
+    borderRadius: 20,
+  },
 });

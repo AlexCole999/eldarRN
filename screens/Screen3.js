4de4d891d22c82d73661cefbc265646812a3bf6e
Leo <62186360+AlexCole999@users.noreply.github.com>
2024-04-12 03:18:21 +0500

    appended images upload logic

--- screens/Screen3.js
+++ screens/Screen3.js
@@ -1,47 +1,68 @@
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, Alert, TextInput, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker';
+import { SaveFormat, manipulateAsync } from 'expo-image-manipulator';
 
 const Screen3 = () => {
-  const navigation = useNavigation();
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
 
-  const handleAddUser = async () => {
-    if (!name || !email) {
-      alert('Пожалуйста, введите имя и email пользователя');
-      return;
-    }
+  const [image, setImage] = useState(null);
+
+  const selectImage = async () => {
     try {
-      await axios.post('http://192.168.1.102:3000/users', { name, email });
-      alert('Пользователь успешно добавлен');
-      setName('');
-      setEmail('');
-    } catch (error) {
-      alert('Ошибка при добавлении пользователя');
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        allowsEditing: true,
+      });
+      if (!result.cancelled) {
+        const manipulatedImage = await manipulateAsync(
+          result.assets[0].uri,
+          [{ resize: { width: 500 } }],
+          { compress: 0.65, format: SaveFormat.JPEG }
+        );
+        console.log(manipulatedImage.uri)
+        await setImage(manipulatedImage.uri)
+      }
+    } catch (err) {
+      console.error('Ошибка при выборе изображения:', err);
+    }
+    console.log('pick')
+  };
+
+  const uploadImage = async () => {
+
+    let imageParsedLength = image.split('/').length
+    let imageName = image.split('/')[imageParsedLength - 1]
+
+    try {
+      let formData = new FormData();
+      formData.append('image', {
+        uri: image,
+        type: 'image/jpeg', // или 'image/png'
+        name: imageName,
+      });
+
+      let response = await axios.post('http://192.168.1.102:3000/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      Alert.alert('Success', response.data.message);
+    } catch (err) {
+      console.error('Ошибка при загрузке изображения:', err);
+      Alert.alert('Error', 'Ошибка при загрузке изображения');
     }
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Добавить пользователя</Text>
-      <TextInput
-        placeholder="Имя"
-        value={name}
-        onChangeText={setName}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        style={styles.input}
-      />
-      <TouchableOpacity style={styles.button} onPress={handleAddUser}>
-        <Text style={styles.buttonText}>Добавить</Text>
-      </TouchableOpacity>
+    <View>
+      {image && <Image source={{ uri: image }} style={{ width: 350, height: 500 }} />}
+      <Button title="Выбрать изображение" onPress={selectImage} />
+      <Button title="Загрузить изображение" onPress={uploadImage} disabled={!image} />
     </View>
   );
 };

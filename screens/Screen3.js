5445a0433c782c64c2d20a3b95c859600385d91f
Leo <62186360+AlexCole999@users.noreply.github.com>
2024-04-17 21:35:25 +0500

    many pictures appended

--- screens/Screen3.js
+++ screens/Screen3.js
@@ -1,30 +1,30 @@
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { StyleSheet, Text, View, Button, Image, Alert, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, Alert, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
 import { SaveFormat, manipulateAsync } from 'expo-image-manipulator';
 
 const Screen3 = () => {
 
-  const [image, setImage] = useState(null);
+  const [images, setImages] = useState([]);
 
   const selectImage = async () => {
     try {
       let result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         quality: 1,
-        allowsEditing: true,
+        multiple: true, // Разрешаем выбор нескольких файлов
       });
       if (!result.cancelled) {
-        const manipulatedImage = await manipulateAsync(
-          result.assets[0].uri,
-          [{ resize: { width: 500 } }],
-          { compress: 0.75, format: SaveFormat.JPEG }
-        );
-        console.log(manipulatedImage.uri)
-        await setImage(manipulatedImage.uri)
+        for (const photo of result.assets) {
+          const manipulatedImage = await manipulateAsync(
+            photo.uri,
+            [{ resize: { width: 500 } }],
+            { compress: 0.75, format: SaveFormat.JPEG }
+          );
+          setImages(prevImages => [...prevImages, manipulatedImage.uri]);
+        }
       }
     } catch (err) {
       console.error('Ошибка при выборе изображения:', err);
@@ -32,38 +32,75 @@ const Screen3 = () => {
     console.log('pick')
   };
 
-  const uploadImage = async () => {
-
-    let imageParsedLength = image.split('/').length
-    let imageName = image.split('/')[imageParsedLength - 1]
-
-    try {
-      let formData = new FormData();
-      formData.append('image', {
-        uri: image,
-        type: 'image/jpeg', // или 'image/png'
-        name: imageName,
-      });
-
-      let response = await axios.post('http://192.168.1.102:3000/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
+  const removeImage = (index) => {
+    Alert.alert(
+      'Удаление изображения',
+      'Вы уверены, что хотите удалить изображение?',
+      [
+        {
+          text: 'Да',
+          onPress: () => {
+            const newImages = images.filter((image, i) => i !== index);
+            setImages(newImages);
+          }
         },
-      });
+        {
+          text: 'Нет',
+          onPress: () => console.log('Отмена удаления'),
+          style: 'cancel',
+        },
+      ],
+      { cancelable: false }
+    );
+  };
 
-      Alert.alert('Success', response.data.message.status);
-      console.log(response.data.message.path)
+  const uploadImages = async () => {
+    try {
+      const results = [];
+      const paths = [];
+
+      for (const image of images) {
+        let imageParsedLength = image.split('/').length;
+        let imageName = image.split('/')[imageParsedLength - 1];
+
+        let formData = new FormData();
+        formData.append('images', {
+          uri: image,
+          type: 'image/jpeg',
+          name: imageName,
+        });
+
+        let response = await axios.post('http://192.168.1.102:3000/upload', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+
+        results.push(response.data.message.status);
+        paths.push(response.data.message.paths);
+      }
+
+      console.log(paths.join('\n'));
     } catch (err) {
       console.error('Ошибка при загрузке изображения:', err);
       Alert.alert('Error', 'Ошибка при загрузке изображения');
     }
+    Alert.alert('Успешно', 'Изображения успешно загружены');
   };
 
   return (
-    <View>
-      {image && <Image source={{ uri: image }} style={{ width: 350, height: 500 }} />}
-      <Button title="Выбрать изображение" onPress={selectImage} />
-      <Button title="Загрузить изображение" onPress={uploadImage} disabled={!image} />
+    <View style={styles.container}>
+      <ScrollView horizontal style={styles.scrollView}>
+        {images.map((image, index) => (
+          <TouchableOpacity key={index} onPress={() => removeImage(index)}>
+            <Image source={{ uri: image }} style={styles.image} />
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+      <View style={styles.buttonContainer}>
+        <Button title="Выбрать изображения" onPress={selectImage} />
+        <Button title="Загрузить изображения" onPress={uploadImages} disabled={images.length === 0} />
+      </View>
     </View>
   );
 };
@@ -76,29 +113,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingHorizontal: 20,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
+  scrollView: {
+    height: 120,
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
+  image: {
+    width: 100,
+    height: 100,
     borderRadius: 5,
-    padding: 10,
-    marginBottom: 15,
-    width: '100%',
+    marginHorizontal: 5,
   },
-  button: {
-    backgroundColor: 'blue',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
   },
 });
 

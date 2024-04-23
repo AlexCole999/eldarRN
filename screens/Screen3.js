ed9f169e387cee91dbc0a448b094bb4ae39fbd3d
Leo <62186360+AlexCole999@users.noreply.github.com>
2024-04-23 22:16:52 +0500

    changed images uploading for only one request

--- screens/Screen3.js
+++ screens/Screen3.js
@@ -5,6 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
 import { SaveFormat, manipulateAsync } from 'expo-image-manipulator';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as FileSystem from 'expo-file-system';
 
 const Screen3 = () => {
 
@@ -55,49 +56,6 @@ const Screen3 = () => {
     );
   };
 
-  // const uploadImages = async () => {
-  //   try {
-
-  //     const userData = await AsyncStorage.getItem('userData')
-
-  //     const results = [];
-  //     const paths = [];
-
-  //     for (const image of images) {
-  //       let imageParsedLength = image.split('/').length;
-  //       let imageName = image.split('/')[imageParsedLength - 1];
-
-  //       let formData = new FormData();
-
-  //       formData.append('user', userData);
-  //       formData.append('image', {
-  //         uri: image,
-  //         type: 'image/jpeg',
-  //         name: imageName,
-  //       });
-
-
-  //       console.log(formData)
-
-
-  //       let response = await axios.post('http://192.168.1.102:3000/upload', formData, {
-  //         headers: {
-  //           'Content-Type': 'multipart/form-data',
-  //         },
-  //       });
-
-  //       results.push(response.data.message.status);
-  //       paths.push(response.data.message.paths);
-  //     }
-
-  //     Alert.alert('Успешно', 'Изображения успешно загружены');
-  //     console.log(paths.join('\n'));
-  //   } catch (err) {
-  //     console.error('Ошибка при загрузке изображения:', err);
-  //     Alert.alert('Error', 'Ошибка при загрузке изображения');
-  //   }
-  // };
-
   const uploadImages = async () => {
     try {
 
@@ -130,7 +88,6 @@ const Screen3 = () => {
       console.error('Ошибка при загрузке изображения:', err.message);
     }
   };
-
 
   return (
     <ScrollView contentContainerStyle={styles.container}>

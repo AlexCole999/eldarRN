8049835aa6ec360efd37e18b872b0fe0b92e3c79
Leo <62186360+AlexCole999@users.noreply.github.com>
2024-04-29 01:21:54 +0500

    improved ProfileScreen for display adsenses whish user already has

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, TouchableOpacity, Alert, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, FlatList, Text, TextInput, View, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
@@ -8,6 +8,7 @@ const ProfileScreen = () => {
   const [phone, setPhone] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+  const [adsenses, setAdsenses] = useState([]);
 
   const handleClearDatabase = async () => {
     Alert.alert(
@@ -34,13 +35,18 @@ const ProfileScreen = () => {
     );
   };
 
-  const handleCustom = () => {
-    axios.post('http://192.168.1.102:3000/users', {
-      name: 'тфьутфьу',
-      phone: '898988989801'
-    })
-      .then(x => console.log(x.data))
-      ;
+  const handleCustom = async () => {
+
+    let user = await AsyncStorage.getItem('userData');
+    if (user) {
+      try {
+        let response = await axios.post('http://192.168.1.102:3000/getUserAdsenses', { user });
+        console.log(response.data.adsenses);
+        setAdsenses(response.data.adsenses); // Устанавливаем полученные объявления в состояние
+      } catch (error) {
+        console.error("Ошибка при получении объявлений:", error);
+      }
+    }
   }
 
   const handleRegistration = async () => {
@@ -68,34 +74,35 @@ const ProfileScreen = () => {
     }
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     loadUserData();
+    handleCustom();
   }, []);
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {userData ? (
         <View>
-          <Text>Имя: {userData.name}</Text>
-          <Text>Телефон: {userData.phone}</Text>
+          <Text style={styles.title}>Имя: {userData?.name}</Text>
+          <Text style={styles.title}>Телефон: {userData?.phone}</Text>
         </View>
       ) : (
         <View>
-          <Text>Регистрация</Text>
+          <Text style={styles.title}>Регистрация</Text>
           <TextInput
-            style={styles.input} // Устанавливаем ширину 90% экрана
+            style={styles.input}
             value={name}
             onChangeText={setName}
             placeholder="Имя"
           />
           <TextInput
-            style={styles.input} // Устанавливаем ширину 90% экрана
+            style={styles.input}
             value={phone}
             onChangeText={setPhone}
             placeholder="Телефон"
           />
           <TextInput
-            style={styles.input} // Устанавливаем ширину 90% экрана
+            style={styles.input}
             value={password}
             onChangeText={setPassword}
             placeholder="Пароль"
@@ -106,18 +113,46 @@ const ProfileScreen = () => {
           </TouchableOpacity>
         </View>
       )}
+      <Text style={styles.title}>Мои объявления</Text>
+      {adsenses.map(ad => (
+        <View key={ad._id} style={styles.adContainer}>
+          <FlatList
+            data={ad.imagesList}
+            keyExtractor={(item, index) => index.toString()}
+            horizontal
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => console.log(`http://192.168.1.102:3000/`)}>
+                <Image source={{ uri: `http://192.168.1.102:3000/${userData?.phone}/${item}` }} style={styles.adImage} />
+              </TouchableOpacity>
+            )}
+          />
+          <View style={styles.infoContainer}>
+            <Text style={styles.adText}>Город: {ad.city}</Text>
+            <Text style={styles.adText}>Категория: {ad.category}</Text>
+            <Text style={styles.adText}>Адрес: {ad.address}</Text>
+            <Text style={styles.adText}>Телефон: {ad?.phone}</Text>
+            <Text style={styles.adText}>Часы работы: {ad.workhours}</Text>
+            {ad.servicesList.map(service => (
+              <Text key={service.price} style={styles.adText}>
+                Часы: {service.hours}, Цена: {service.price}
+              </Text>
+            ))}
+          </View>
+        </View>
+      ))}
+
       <TouchableOpacity style={styles.button} onPress={handleClearDatabase}>
         <Text style={styles.buttonText}>Очистить базу данных</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={handleCustom}>
-        <Text style={styles.buttonText}>Custom</Text>
+        <Text style={styles.buttonText}>Обновить данные объявлений</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={styles.button}
         onPress={async () => {
           await AsyncStorage.clear();
           setUserData(null);
-
+          setAdsenses([])
         }}>
         <Text style={styles.buttonText}>Удалить AsyncStorage</Text>
       </TouchableOpacity>
@@ -125,18 +160,17 @@ const ProfileScreen = () => {
   );
 };
 
-
-
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    justifyContent: 'center',
     backgroundColor: '#fff',
     paddingHorizontal: 20,
+    paddingTop: 20,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    textAlign: 'center',
     marginBottom: 20,
   },
   input: {
@@ -153,13 +187,36 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     borderRadius: 5,
     marginVertical: 10,
+    alignItems: 'center',
   },
   buttonText: {
     color: '#fff',
     fontSize: 18,
     fontWeight: 'bold',
   },
-
+  adContainer: {
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 20,
+  },
+  adText: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  adImage: {
+    width: 290,
+    height: 300,
+    resizeMode: 'cover',
+    marginBottom: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'lightgray',
+  },
+  infoContainer: {
+    paddingHorizontal: 10,
+  },
 });
 
 export default ProfileScreen;

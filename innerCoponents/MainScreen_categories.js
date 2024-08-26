af54b81c904431221e416e5268697daee99a10da
Leo <62186360+AlexCole999@users.noreply.github.com>
2024-08-26 23:04:33 +0500

    MainScreen design DONE

--- innerCoponents/MainScreen_categories.js
+++ innerCoponents/MainScreen_categories.js
@@ -56,13 +56,87 @@ const categoriesList1 = [
   }
 ];
 
+const categories = [
+  {
+    name: 'Косметология',
+    icon: 'categories_cosmetology.jpg',
+    subcategories: [
+      { name: 'Эстетическая косметология', icon: 'categories_cosmetology.jpg' },
+      { name: 'Аппаратная косметология', icon: 'categories_cosmetology.jpg' },
+      { name: 'Инъекционная косметология', icon: 'categories_cosmetology.jpg' },
+      { name: 'Депиляция, шугаринг', icon: 'categories_cosmetology.jpg' },
+      { name: 'Перманентный макияж', icon: 'categories_cosmetology.jpg' }
+    ]
+  },
+  { name: 'Лашмейкеры и Бровисты', icon: 'categories_lashmaker.jpg' },
+  {
+    name: 'Стилисты',
+    icon: 'categories_stylist.jpg',
+    subcategories: [
+      { name: 'Стилисты по волосам', icon: 'categories_stylist.jpg' },
+      { name: 'Визаж', icon: 'categories_stylist.jpg' },
+      { name: 'Барбершоп', icon: 'categories_stylist.jpg' }
+    ]
+  },
+  { name: 'Ногтевой сервис', icon: 'categories_nails.jpg' },
+  { name: 'Массаж и Спа', icon: 'categories_massageAndSpa.jpg' },
+  { name: 'Тату и Пирсинг', icon: 'categories_tattooAndPiercing.jpg' },
+  { name: 'Коворкинг и Конферент залы', icon: 'categories_coworking.jpg' },
+  { name: 'Фотостудия', icon: 'categories_photostudy.jpg' },
+  { name: 'Рестораны и Банкетные Залы', icon: 'categories_bankets.jpg' },
+  { name: 'Прочее', icon: 'categories_cosmetology.jpg' } // Используем иконку "Все"
+];
+
 const CategoriesInnerComponent = () => {
 
   const dispatch = useDispatch();
   const navigation = useNavigation();
   const screenWidth = Dimensions.get('window').width;
 
+  const [selectedCategory, setSelectedCategory] = useState('Все');
+
   const [selectedIndex, setSelectedIndex] = useState(null);
+
+  const [showAll, setShowAll] = useState(false);
+
+  let itemsToRender = [];
+
+  const renderCategories = () => {
+
+
+    if (selectedCategory === 'Все') {
+      itemsToRender = categories.flatMap(category =>
+        category.subcategories ? category.subcategories : [category]
+      );
+    } else {
+      const selectedCat = categories.find(category => category.name === selectedCategory);
+      itemsToRender = selectedCat?.subcategories || [selectedCat];
+    }
+
+    const itemsToShow = showAll ? itemsToRender : itemsToRender.slice(0, 6);
+
+    return (
+      <>
+        {itemsToShow.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            style={{ borderRadius: 12, padding: 8, backgroundColor: 'white', elevation: 4 }}
+            onPress={() => { dispatch(filterSlice.actions.filterCategory(item.name)); navigation.navigate('Каталог') }}
+          >
+            <Image
+              source={{ uri: `${localhosturl}/categoryPhotos/${item.icon}` }}
+              style={{ width: 136, height: 100, borderRadius: 12 }}
+              resizeMode='stretch'
+            />
+            <View style={{ display: 'flex', flex: 1, flexDirection: 'row', alignItems: 'start', justifyContent: 'start', borderRadius: 5 }}>
+              <Text style={{ color: '#333333', textAlign: 'left', paddingTop: 6, fontFamily: 'Manrope_500Medium', fontSize: 14, maxWidth: 130 }}>{item.name}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </>
+    );
+  };
+
 
   return (
     <View >
@@ -111,17 +185,17 @@ const CategoriesInnerComponent = () => {
           data={categoriesList1}
           renderItem={({ item, index }) => {
             const isLastItem = index === categoriesList1.length - 1;
-            const isSelected = selectedIndex === index; // Проверяем, выбран ли элемент
+            const isSelected = selectedCategory === item.name;
             return (
               <TouchableOpacity
                 style={{ height: 48, backgroundColor: 'white', flexDirection: 'row', alignItems: 'center', marginRight: !isLastItem ? 12 : 40, padding: 8, borderRadius: 12, gap: 6, opacity: isSelected ? 1 : 0.5 }}
-                onPress={() => setSelectedIndex(index)}
+                onPress={() => { setSelectedCategory(item.name); setShowAll(false) }}
               >
                 <View style={{ height: 32, width: 32, borderRadius: 6 }}>
                   <Image
                     source={{ uri: `${localhosturl}/categoryPhotos/${item.icon}` }}
                     style={{ width: '100%', height: '100%', borderRadius: 6 }}
-                    resizeMode="stretch"  // Растягивает изображение до заданных размеров
+                    resizeMode="cover"
                   />
                 </View>
                 <Text style={{ fontFamily: 'Manrope_500Medium', color: '#333333', fontSize: 16 }}>{item.name}</Text>
@@ -136,30 +210,37 @@ const CategoriesInnerComponent = () => {
       </View>
 
       <View style={{ paddingLeft: 24, marginTop: -24, flexDirection: 'row', flexWrap: 'wrap', gap: 8 }}>
-
-        {
-          categoriesList1.map((item) => {
-            return (
-              <TouchableOpacity
-                style={{ borderRadius: 12, padding: 8, backgroundColor: 'white', elevation: 4 }}
-                key={item.name}
-                onPress={() => { dispatch(filterSlice.actions.filterCategory(item.name)); navigation.navigate('Каталог') }}
-              >
-                <Image
-                  source={{ uri: `${localhosturl}/categoryPhotos/${item.icon}` }}
-                  style={{ width: 136, height: 100, borderRadius: 12 }}
-                  resizeMode='stretch'
-                >
-
-                </Image>
-                <View style={{ display: 'flex', flex: 1, flexDirection: 'row', alignItems: 'start', justifyContent: 'start', borderRadius: 5 }}>
-                  <Text style={{ color: '#333333', textAlign: 'left', paddingTop: 6, fontFamily: 'Manrope_500Medium', fontSize: 14, maxWidth: 100 }}>{item.name}</Text>
-                </View>
-              </TouchableOpacity>
-            )
-          })
-        }
+        {renderCategories()}
       </View>
+
+      {!showAll && itemsToRender.length > 6 && (
+        <View
+          style={{
+            marginTop: 24,
+            marginHorizontal: 24,
+            borderRadius: 12,
+            elevation: 4,
+            height: 36
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              backgroundColor: 'white',
+              borderColor: 'rgba(0,148,255,0.9)',
+              borderWidth: 1,
+              borderRadius: 12,
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: 36
+            }}
+            onPress={() => setShowAll(true)}
+          >
+            <Text style={{ color: '#333333', fontFamily: 'Manrope_500Medium', fontSize: 16, paddingBottom: 2 }}>
+              Показать больше
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View >
   );
 };

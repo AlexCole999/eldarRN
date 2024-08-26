6fc65666bb9b8a1e4ade5cb1f6fcd729dadd7eca
Leo <62186360+AlexCole999@users.noreply.github.com>
2024-08-26 22:07:38 +0500

    Mainscreen_categories working

--- innerCoponents/MainScreen_categories.js
+++ innerCoponents/MainScreen_categories.js
@@ -8,49 +8,52 @@ import { useSelector, useDispatch } from 'react-redux';
 import { createSlice } from '@reduxjs/toolkit';
 import filterSlice from '../storage/filterSlice';
 
-const categoriesList = [
-  [
-    {
-      name: 'Косметология',
-      icon: 'categories_cosmetology.jpg'
-    },
-    {
-      name: 'Лашмейкеры и Бровисты',
-      icon: 'categories_lashmaker.jpg'
-    },
-    {
-      name: 'Стилисты',
-      icon: 'categories_stylist.jpg'
-    }
-  ],
-  [
-    {
-      name: 'Ногтевой сервис',
-      icon: 'categories_nails.jpg'
-    },
-    {
-      name: 'Массаж и Спа',
-      icon: 'categories_massageAndSpa.jpg'
-    },
-    {
-      name: 'Тату и Пирсинг',
-      icon: 'categories_tattooAndPiercing.jpg'
-    }
-  ],
-  [
-    {
-      name: 'Коворкинг и Конферент залы',
-      icon: 'categories_coworking.jpg'
-    },
-    {
-      name: 'Фотостудия',
-      icon: 'categories_photostudy.jpg'
-    },
-    {
-      name: 'Рестораны и Банкетные Залы',
-      icon: 'categories_bankets.jpg'
-    }
-  ]
+import Filter from '../assets/Filter.png'
+import Logo from '../assets/Logo.png'
+import main_notifications from '../assets/main_notifications.png'
+import { isNewBackTitleImplementation } from 'react-native-screens';
+
+const categoriesList1 = [
+  {
+    name: 'Все',
+    icon: 'categories_cosmetology.jpg'
+  },
+  {
+    name: 'Косметология',
+    icon: 'categories_cosmetology.jpg'
+  },
+  {
+    name: 'Лашмейкеры и Бровисты',
+    icon: 'categories_lashmaker.jpg'
+  },
+  {
+    name: 'Стилисты',
+    icon: 'categories_stylist.jpg'
+  },
+  {
+    name: 'Ногтевой сервис',
+    icon: 'categories_nails.jpg'
+  },
+  {
+    name: 'Массаж и Спа',
+    icon: 'categories_massageAndSpa.jpg'
+  },
+  {
+    name: 'Тату и Пирсинг',
+    icon: 'categories_tattooAndPiercing.jpg'
+  },
+  {
+    name: 'Коворкинг и Конферент залы',
+    icon: 'categories_coworking.jpg'
+  },
+  {
+    name: 'Фотостудия',
+    icon: 'categories_photostudy.jpg'
+  },
+  {
+    name: 'Рестораны и Банкетные Залы',
+    icon: 'categories_bankets.jpg'
+  }
 ];
 
 const CategoriesInnerComponent = () => {
@@ -59,77 +62,105 @@ const CategoriesInnerComponent = () => {
   const navigation = useNavigation();
   const screenWidth = Dimensions.get('window').width;
 
-  const renderItem = ({ item, index }) => (
-    <View style={{ display: 'flex', rowGap: 10, marginRight: 10 }}>
-      <TouchableOpacity
-        onPress={() => { dispatch(filterSlice.actions.filterCategory(item[0]?.name)); navigation.navigate('Каталог') }}
-      >
-        <ImageBackground
-          source={{ uri: `${localhosturl}/categoryPhotos/${item[0].icon}` }}
-          style={{ width: screenWidth * 0.45, height: 100, borderRadius: 5 }}
-          resizeMode='stretch'
-          imageStyle={{ borderRadius: 5, width: '100%', height: '100%' }}
-        >
-          <View style={{ backgroundColor: 'rgba(0,0,0, 0.40)', display: 'flex', flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', borderRadius: 5 }}>
-            <Text style={{ color: 'white', paddingHorizontal: 10, textAlign: 'center', fontWeight: 600 }}>{item[0]?.name}</Text>
-          </View>
-        </ImageBackground>
-      </TouchableOpacity>
-      {item[1]?.name
-        ? <TouchableOpacity
-          onPress={() => { dispatch(filterSlice.actions.filterCategory(item[1]?.name)); navigation.navigate('Каталог') }}
-        >
-          <ImageBackground
-            source={{ uri: `${localhosturl}/categoryPhotos/${item[1]?.icon}` }}
-            style={{ width: screenWidth * 0.45, height: 100, borderRadius: 5 }}
-            resizeMode='stretch'
-            imageStyle={{ borderRadius: 5, width: '100%', height: '100%' }}
-          >
-            <View style={{ backgroundColor: 'rgba(0,0,0, 0.40)', display: 'flex', flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', borderRadius: 5 }}>
-              <Text style={{ color: 'white', paddingHorizontal: 10, textAlign: 'center', fontWeight: 600 }}>{item[1]?.name}</Text>
-            </View>
-          </ImageBackground>
-        </TouchableOpacity>
-        : null}
-      {item[2]?.name
-        ? <TouchableOpacity
-          onPress={() => { dispatch(filterSlice.actions.filterCategory(item[2]?.name)); navigation.navigate('Каталог') }}
-        >
-          <ImageBackground
-            source={{ uri: `${localhosturl}/categoryPhotos/${item[2]?.icon}` }}
-            style={{ width: screenWidth * 0.45, height: 100, borderRadius: 5 }}
-            resizeMode='stretch'
-            imageStyle={{ borderRadius: 5, width: '100%', height: '100%' }}
-          >
-            <View style={{ backgroundColor: 'rgba(0,0,0, 0.40)', display: 'flex', flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', borderRadius: 5 }}>
-              <Text style={{ color: 'white', paddingHorizontal: 10, textAlign: 'center', fontWeight: 600 }}>{item[2]?.name}</Text>
-            </View>
-          </ImageBackground>
-        </TouchableOpacity>
-        : null}
-    </View>
-  );
-
-  // Добавляем компонент во всю высоту
-  const renderLastItem = () => (
-    <TouchableOpacity style={{ width: screenWidth * 0.4, backgroundColor: 'rgba(128, 128, 128, 0.5)', justifyContent: 'center', alignItems: 'center', borderRadius: 5 }}
-      onPress={() => { dispatch(filterSlice.actions.filterCategory('Прочее')); navigation.navigate('Каталог') }}
-    >
-      <Text style={{ color: 'white', padding: 10, textAlign: 'center', fontWeight: 600 }}>Прочее</Text>
-    </TouchableOpacity>
-  );
+  const [selectedIndex, setSelectedIndex] = useState(null);
 
   return (
-    <View style={{ paddingHorizontal: 20, paddingVertical: 10 }}>
-      <Text style={{ fontSize: 16, paddingBottom: 10, fontWeight: 700 }}>Категории</Text>
-      <FlatList
-        data={[...categoriesList, null]} // Добавляем null как последний элемент для рендера
-        renderItem={({ item, index }) => item ? renderItem({ item, index }) : renderLastItem()}
-        keyExtractor={(item, index) => item ? item[0].name : `last-item-${index}`}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-      />
-    </View>
+    <View >
+
+      <View style={{ backgroundColor: 'rgba(0, 148, 255,1);', minHeight: 232, borderBottomLeftRadius: 24, borderBottomRightRadius: 24 }}>
+
+        <View style={{ paddingTop: 20, paddingHorizontal: 24, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+
+          <View style={{ flexDirection: 'row', gap: 4, alignItems: 'center', paddingTop: 25 }}>
+            <TouchableOpacity
+              style={{ height: 32, width: 32 }}
+            >
+              <Image
+                source={Logo}
+                style={{ width: '100%', height: '100%' }}
+                resizeMode="stretch"  // Растягивает изображение до заданных размеров
+              />
+
+            </TouchableOpacity>
+
+            <Text style={{ fontFamily: 'Manrope_600SemiBold', color: 'white', fontSize: 24, textAlign: 'left' }}>Arenda Go</Text>
+
+          </View>
+
+          <View style={{ paddingTop: 25 }}>
+            <TouchableOpacity
+              style={{ height: 32, width: 32 }}
+            >
+              <Image
+                source={main_notifications}
+                style={{ width: '100%', height: '100%' }}
+                resizeMode="stretch"  // Растягивает изображение до заданных размеров
+              />
+
+            </TouchableOpacity>
+          </View>
+
+        </View>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 24, paddingTop: 31 }}>
+          <Text style={{ fontFamily: 'Manrope_600SemiBold', color: 'white', fontSize: 24, textAlign: 'left' }}>Категории</Text>
+        </View>
+
+        <FlatList
+          style={{ paddingLeft: 24, paddingTop: 24, paddingBottom: 40 }}
+          data={categoriesList1}
+          renderItem={({ item, index }) => {
+            const isLastItem = index === categoriesList1.length - 1;
+            const isSelected = selectedIndex === index; // Проверяем, выбран ли элемент
+            return (
+              <TouchableOpacity
+                style={{ height: 48, backgroundColor: 'white', flexDirection: 'row', alignItems: 'center', marginRight: !isLastItem ? 12 : 40, padding: 8, borderRadius: 12, gap: 6, opacity: isSelected ? 1 : 0.5 }}
+                onPress={() => setSelectedIndex(index)}
+              >
+                <View style={{ height: 32, width: 32, borderRadius: 6 }}>
+                  <Image
+                    source={{ uri: `${localhosturl}/categoryPhotos/${item.icon}` }}
+                    style={{ width: '100%', height: '100%', borderRadius: 6 }}
+                    resizeMode="stretch"  // Растягивает изображение до заданных размеров
+                  />
+                </View>
+                <Text style={{ fontFamily: 'Manrope_500Medium', color: '#333333', fontSize: 16 }}>{item.name}</Text>
+              </TouchableOpacity>
+            )
+          }}
+          keyExtractor={(item, index) => index}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+
+      </View>
+
+      <View style={{ paddingLeft: 24, marginTop: -24, flexDirection: 'row', flexWrap: 'wrap', gap: 8 }}>
+
+        {
+          categoriesList1.map((item) => {
+            return (
+              <TouchableOpacity
+                style={{ borderRadius: 12, padding: 8, backgroundColor: 'white', elevation: 4 }}
+                key={item.name}
+                onPress={() => { dispatch(filterSlice.actions.filterCategory(item.name)); navigation.navigate('Каталог') }}
+              >
+                <Image
+                  source={{ uri: `${localhosturl}/categoryPhotos/${item.icon}` }}
+                  style={{ width: 136, height: 100, borderRadius: 12 }}
+                  resizeMode='stretch'
+                >
+
+                </Image>
+                <View style={{ display: 'flex', flex: 1, flexDirection: 'row', alignItems: 'start', justifyContent: 'start', borderRadius: 5 }}>
+                  <Text style={{ color: '#333333', textAlign: 'left', paddingTop: 6, fontFamily: 'Manrope_500Medium', fontSize: 14, maxWidth: 100 }}>{item.name}</Text>
+                </View>
+              </TouchableOpacity>
+            )
+          })
+        }
+      </View>
+    </View >
   );
 };
 
